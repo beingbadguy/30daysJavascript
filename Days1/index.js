b81e1ed44d08a33e150cbf8bf118fc11abd7dff3
Aman Kumar <authorisedaman@gmail.com>
2024-07-13 21:46:13 +0530

    day1

---
+++ Days1/index.js
@@ -0,0 +1,52 @@
+// Day 1 : variables and dataTypes
+
+// variable declaration
+var age = 30;
+console.log(age);
+
+let text = "Once upon a time there was a guy named anonymous.";
+console.log(text);
+
+// constant declaration
+const isMarried = false;
+console.log(isMarried);
+
+// Data Types
+let Lastname = "yadav";
+let ageInYears = 21;
+let isEmployed = false;
+let audi = {
+  color: "red",
+  model: "2024",
+  isAlloyWheel: true,
+};
+
+let footballsGoats = [
+  "CristianoRonaldo",
+  "Thierry Henry",
+  "Neymar",
+  "Lionel messi",
+  "zinedine zidane",
+  "Ronaldinho",
+];
+
+console.log(typeof Lastname);
+console.log(typeof age);
+console.log(typeof isEmployed);
+console.log(typeof audi);
+console.log(typeof footballsGoats);
+
+// reassigning values
+
+let poem = "This is a sample poem.";
+console.log(poem);
+poem = "This is a new version of the pome";
+console.log(poem);
+
+
+// Understanding const
+
+const newAge = 43;
+newAge = 44;
+console.log(newAge);
+
